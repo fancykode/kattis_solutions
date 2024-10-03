9459b184b2c51736ab7f8e1a3543b1b610d2b266
ciderpie <>
2024-10-03 19:36:45 +0300

    Stand on Zanzibar problem

---
+++ zanzibar.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for _ in range(T):
+    result = 0
+    line_parts = input().split(' ')
+    for i in range(1, len(line_parts) - 1):
+        prev = int(line_parts[i - 1])
+        curr = int(line_parts[i])
+        if prev*2 <= curr:
+            result += curr - prev*2
+    print(result)
