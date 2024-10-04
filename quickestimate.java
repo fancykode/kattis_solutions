098b56d62e5da69182468bb30f69b5b88d4d793f
ciderpie <>
2024-10-04 21:57:28 +0300

    Quick Estimates

---
+++ quickestimate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class quickestimate {
+
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < N; i++) {
+            String eCost = sc.nextLine();
+            System.out.println(eCost.length());
+        }
+    }
+}
