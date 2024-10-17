4b3b277d90f719692299768bd61363a143ef914a
fancykode <james_j439@protonmail.com>
2024-10-17 10:57:27 +0300

    Some Sum

---
+++ somesum.py
@@ -0,0 +1,23 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+if N == 1:
+    print("Either")
+elif N == 2:
+    print("Odd")
+elif N == 3:
+    print("Either")
+elif N == 4:
+    print("Even")
+elif N == 5:
+    print("Either")
+elif N == 6:
+    print("Odd")
+elif N == 7:
+    print("Either")
+elif N == 8:
+    print("Even")
+elif N == 9:
+    print("Either")
+elif N == 10:
+    print("Odd")
