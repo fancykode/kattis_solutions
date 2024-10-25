eae8d43b30dec216cec33b144c2c2b87210ad229
fancykode <james_j439@protonmail.com>
2024-10-25 13:29:03 +0300

    perf: use slice instead of map to store positions of keys of the keyboard

--- touchscreenkeyboard.go
+++ touchscreenkeyboard.go
@@ -3,15 +3,24 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
 )
 
-func main() {
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
 
+func main() {
+	type Pos struct {
+		I int
+		J int
+	}
 	type WordDist struct {
 		Distance int
 		Word     string
@@ -23,13 +32,12 @@ func main() {
 		"zxcvbnm",
 	}
 
-	letters := map[byte][]int{}
+	var letters [26]Pos
 	for i := 0; i < len(keyboard); i++ {
 		for j := 0; j < len(keyboard[i]); j++ {
-			letters[keyboard[i][j]] = []int{i, j}
+			letters[keyboard[i][j]-'a'] = Pos{I: i, J: j}
 		}
 	}
-
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	scanner.Text()
@@ -38,15 +46,16 @@ func main() {
 		lineParts := strings.Split(scanner.Text(), " ")
 		word := lineParts[0]
 		l, _ := strconv.Atoi(lineParts[1])
+
 		scList := []WordDist{}
 		for i := 0; i < l; i++ {
 			scanner.Scan()
 			w := scanner.Text()
 			sumDist := 0
 			for j := 0; j < len(w); j++ {
-				p1 := letters[w[j]]
-				p2 := letters[word[j]]
-				dist := int(math.Abs(float64((p1[0] - p2[0]))) + math.Abs(float64(p1[1]-p2[1])))
+				p1 := letters[w[j]-'a']
+				p2 := letters[word[j]-'a']
+				dist := abs(p1.I-p2.I) + abs(p1.J-p2.J)
 				sumDist += dist
 			}
 			scList = append(scList, WordDist{Distance: sumDist, Word: w})
@@ -58,7 +67,7 @@ func main() {
 			return scList[i].Word < scList[j].Word
 		})
 		for j := 0; j < len(scList); j++ {
-			fmt.Printf("%s %d\n", scList[j].Word, scList[j].Distance)
+			fmt.Println(scList[j].Word + " " + strconv.Itoa(scList[j].Distance))
 		}
 	}
 }
