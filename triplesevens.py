887e14327f0e5daac4e0f16cd0fbe190e44964fb
fancykode <james_j439@protonmail.com>
2024-11-13 14:14:56 +0300

    Triple Sevens

---
+++ triplesevens.py
@@ -0,0 +1,9 @@
+n = int(input())
+w1 = [int(i) for i in input().split(" ")]
+w2 = [int(i) for i in input().split(" ")]
+w3 = [int(i) for i in input().split(" ")]
+
+if (7 in w1) and (7 in w2) and (7 in w3):
+    print(777)
+else:
+    print(0)
