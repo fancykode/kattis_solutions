89dc6ba43dde1cc7b42f75736d5b71b6423b13f2
fancykode <james_j439@protonmail.com>
2024-11-16 12:32:04 +0300

    Veður - Vindhraði

---
+++ vedurvindhradi.py
@@ -0,0 +1,29 @@
+k = float(input())
+k = int(k * 10)
+
+if k >= 0 and k <= 2:
+    print("Logn")
+elif k >= 3 and k <= 15:
+    print("Andvari")
+elif k >= 16 and k <= 33:
+    print("Kul")
+elif k >= 34 and k <= 54:
+    print("Gola")
+elif k >= 55 and k <= 79:
+    print("Stinningsgola")
+elif k >= 80 and k <= 107:
+    print("Kaldi")
+elif k >= 108 and k <= 138:
+    print("Stinningskaldi")
+elif k >= 139 and k <= 171:
+    print("Allhvass vindur")
+elif k >= 172 and k <= 207:
+    print("Hvassvidri")
+elif k >= 208 and k <= 244:
+    print("Stormur")
+elif k >= 245 and k <= 284:
+    print("Rok")
+elif k >= 285 and k <= 326:
+    print("Ofsavedur")
+elif k >= 327:
+    print("Farvidri")
