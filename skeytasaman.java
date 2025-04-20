00be17680119de647cccfe6d3971dceb766887b4
fancykode <james_j439@protonmail.com>
2025-04-20 17:08:41 +0200

    Concatenate

---
+++ skeytasaman.java
@@ -0,0 +1,20 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+public class skeytasaman {
+
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        OutputStream out = new BufferedOutputStream(System.out);
+        // var out = new PrintWriter(new BufferedOutputStream(System.out));
+        StringBuilder result = new StringBuilder();
+        result.append(in.readLine());
+        result.append(in.readLine());
+
+        out.write(result.toString().getBytes());
+        out.flush();
+    }
+}
