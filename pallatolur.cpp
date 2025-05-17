6fc9b37c4f79cce956ca24491acc4b5ac2bc773e
fancykode <james_j439@protonmail.com>
2025-05-17 14:37:51 +0200

    Pallatölur

---
+++ pallatolur.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int a, b;
+  cin >> a >> b;
+  if (2 <= b && 2 >= a) {
+    cout << "1\n2\n";
+  } else {
+    cout << ":(\n";
+  }
+  return 0;
+}
