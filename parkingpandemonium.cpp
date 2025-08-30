1e28d24d0913e0b85fd27152a273a519f9b213b0
fancykode <james_j439@protonmail.com>
2025-08-30 19:39:30 +0200

    Parking Pandemonium

---
+++ parkingpandemonium.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int M, K, C;
+  cin >> M >> K >> C;
+  cout << M * C << "\n";
+  return 0;
+}
