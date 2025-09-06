589e327efd8055f5349c58cb6f1d73b57b1779cf
fancykode <james_j439@protonmail.com>
2025-09-06 12:43:08 +0200

    Flatter Land

---
+++ flatterland.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int n, x;
+  cin >> n >> x;
+  cout << (n - 1) * x << "\n";
+
+  return 0;
+}
