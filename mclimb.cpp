5b2809af6920ae703eaf373a3a3a07667667f399
fancykode <james_j439@protonmail.com>
2025-09-12 20:22:32 +0200

    M-Climb

---
+++ mclimb.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int m, n, y;
+  cin >> m >> n >> y;
+  cout << m * n << "\n";
+  return 0;
+}
