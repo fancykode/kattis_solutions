6999d5654631ba672b39420ff9268998c93be252
fancykode <james_j439@protonmail.com>
2025-09-30 18:30:30 +0200

    Class Photo

---
+++ classphoto.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int w, l;
+  cin >> w >> l;
+  cout << w * l << "\n";
+  return 0;
+}
