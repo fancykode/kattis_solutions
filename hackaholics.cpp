a8fb5e77a27dbcefdd59741bb2806970a032592e
fancykode <james_j439@protonmail.com>
2025-10-23 23:28:40 +0200

    Hack-a-Holics

---
+++ hackaholics.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int N, C, P;
+  cin >> N >> C >> P;
+  cout << N * P << "\n";
+  return 0;
+}
