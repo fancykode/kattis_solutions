8af57825e660a46b945080e71c30e40f4be667d0
fancykode <james_j439@protonmail.com>
2025-10-24 23:39:32 +0200

    13 Floors

---
+++ 13floors.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int x;
+  cin >> x;
+  if (x >= 13) {
+    cout << x + 1 << "\n";
+  } else {
+    cout << x << "\n";
+  }
+  return 0;
+}
