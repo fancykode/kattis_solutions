97196aa243b87ed5d22990b0996b355ab065ffab
fancykode <james_j439@protonmail.com>
2026-09-03 01:37:43 +0300

    Largest Factor

---
+++ largestfactor.py
@@ -0,0 +1,22 @@
+import math
+
+
+def prime_factors(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    # n is odd here
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+        i += 2
+    if n > 2:
+        factors.append(n)
+    return factors
+
+
+n = int(input())
+print(max(prime_factors(n)))
