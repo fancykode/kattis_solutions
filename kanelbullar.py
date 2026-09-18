afd8dcbec74ad0edc8438551a26f54d195995d50
fancykode <james_j439@protonmail.com>
2026-09-18 13:56:36 +0300

    Kanelbullar

---
+++ kanelbullar.py
@@ -0,0 +1,13 @@
+f = int(input())
+y = int(input())
+b = int(input())
+m = int(input())
+s = int(input())
+
+fh = int(input())
+yh = int(input())
+bh = int(input())
+mh = int(input())
+sh = int(input())
+
+print(min([fh // f, yh // y, bh // b, mh // m, sh // s]))
